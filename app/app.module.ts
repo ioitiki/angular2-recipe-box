036a3efa6ae44fbb53400a41827d4b734f7df9c1
Alex Lee and Andrew Malkin <andrew@>
2017-05-02 14:11:19 -0700

    set up angularfire2 so that the database is stored via firebase

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCIbRuMzcaYIN_cJqSrGo0AdplJKC6wFKw",
+  authDomain: "recipe-box-f6b0c.firebaseapp.com",
+  databaseURL: "https://recipe-box-f6b0c.firebaseio.com",
+  projectId: "recipe-box-f6b0c",
+  storageBucket: "recipe-box-f6b0c.appspot.com",
+  messagingSenderId: "979909186719"
+};
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, AngularFireModule.initializeApp(firebaseConfig) ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
